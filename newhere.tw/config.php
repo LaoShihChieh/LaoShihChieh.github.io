2ab0ed4b071616e4affb0a2000c75996b8cef931
LaoShihChieh <66504390+LaoShihChieh@users.noreply.github.com>
2022-01-13 13:54:56 +0800

    Delete newhere.tw directory

--- newhere.tw/config.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	session_start();
-	// connect to database
-	$conn = mysqli_connect("localhost", "root", "Roygbiv1", "Newhere");
-
-	if (!$conn) {
-		die("Error connecting to database: " . mysqli_connect_error());
-  }
-?>
--- newhere.tw/includes/views.php
+++
@@ -1,7 +0,0 @@
-<?php
-$sql = "SELECT views FROM articles WHERE article_slug = '$article_slug'";
-$views =  (int)mysqli_query($conn, $sql);
-$views++;
-$sql = "UPDATE articles SET views = '$views' WHERE article_slug = '$article_slug'";
-mysqli_query($conn, $sql);
-?>
